077e6fe4939dd7a838a688dc8e05e64118ab251a
YourDaniil <01.daniy.01@gmail.com>
2025-04-06 15:13:58 +0300

    new objects: sc, x, y

--- Main.java
+++ Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
     }
     public static int plus(int x, int y) {
         return x + y;
