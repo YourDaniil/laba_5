c3f45b6760f64727b4843b9b1fc0f295a23b7cea
YourDaniil <01.daniy.01@gmail.com>
2025-04-06 15:14:41 +0300

    sout -> plus(x, y), minus(x,y)

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
+
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
+
+        System.out.println("x + y =" + plus(x, y));
+        System.out.println("x - y =" + minus(x, y));
+
     }
     public static int plus(int x, int y) {
         return x + y;
