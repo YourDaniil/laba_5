d02c16064642a182d4262878fb815a6ff9da9ef4
YourDaniil <01.daniy.01@gmail.com>
2025-04-06 15:13:30 +0300

    new functions: plus, minus

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
+    }
+    public static int plus(int x, int y) {
+        return x + y;
+    }
+    public static int minus(int x, int y) {
+        return x - y;
     }
 }
